fb83b6adefaedf9f7499e758f25fe1d9d95f9d2e
horizenight <kshitijroodkee1@gmail.com>
2023-05-25 17:19:44 +0530

    index file made

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::io;
-use std::fs::{File,self};
-use std::path::Path;
+use std::{io, fs};
+use std::fs::File;
+use std::path::{Path,PathBuf};
 use xml::reader::{XmlEvent,EventReader};
 use std::collections::HashMap;
+use serde_json::Result;
 
 #[derive(Debug)]
 struct Lexer<'a>{
@@ -20,6 +21,22 @@ impl <'a> Lexer<'a>{
             self.content = &self.content[1..];
         }
     }
+
+    fn chop(&mut self, n :usize ) -> &'a[char]{
+        let token =  &self.content[0..n];
+        self.content = &self.content[n..];
+        token
+    }
+
+    fn chop_while<P>(&mut self,mut predicate:P) -> &'a[char] where P: FnMut(&char) -> bool {
+        let mut n = 0;
+        while n < self.content.len() && predicate(&self.content[n]){
+            n +=1 ;
+
+        }
+        self.chop(n)
+    }
+
     fn next_token(&mut self) -> Option<&'a [char]>{
         // trim whitespaces from the left 
         self.trim_left();
@@ -31,21 +48,27 @@ impl <'a> Lexer<'a>{
         // 1000th => 1000 th 
         // hello420 => hello420
 
-        if self.content[0].is_alphabetic(){
-            let mut n = 0;
-            while n < self.content.len() && self.content[n].is_alphanumeric(){
-                n +=1 ;
+        if self.content[0].is_numeric(){
+           
+            return Some(self.chop_while(|x| x.is_numeric()));
 
-            }
-            let token = &self.content[0..n];
-            self.content = &self.content[n..];
 
-            return Some(token);
+           
         }
+
+        if self.content[0].is_alphabetic(){
+            return Some(self.chop_while(|x| x.is_alphanumeric()));
+
+            
+        }
+        return Some(self.chop(1));
         
-        todo!()
+
+        
+        // todo!("Invalid token starts with {start}",start = self.content[0])
     }
 }
+
 
 
 impl <'a> Iterator for Lexer<'a>{
@@ -69,6 +92,7 @@ fn read_entire_xml_file<P:AsRef<Path>>(file_path:P) -> io::Result<String>{
     for event in er.into_iter(){
         if let XmlEvent::Characters(text) = event.expect("TODO") {
             content.push_str(&text);
+            content.push_str(" ");
         
         }
     }
@@ -76,24 +100,70 @@ fn read_entire_xml_file<P:AsRef<Path>>(file_path:P) -> io::Result<String>{
 }
 
 
-fn main() -> io::Result<()> {
+type TermFreq = HashMap::<String,usize>;
+type TermFreqIndex = HashMap<PathBuf,TermFreq>;
 
-    let content = read_entire_xml_file("docs.gl/gl4/glVertexAttribDivisor.xhtml")?.chars().collect::<Vec<_>>();
 
-    for token in Lexer::new(&content){
+fn main() -> io::Result<()>{
+    let index_path = "index.json";
+    let index_file = File::open(index_path)?;
+    println!("Reading {index_path} index file...");
+    let tf_index : TermFreqIndex = serde_json::from_reader(index_file).expect("serde does not fail");
+    println!("{index_path} contains {count} files", count = tf_index.len());
+    Ok(())
+}
 
-        println!("{token:?}");
-    }
-
+fn main2() -> io::Result<()> {
+    
     // let all_documents = HashMap::<Path,HashMap<String,usize>> ::new();
 
-    // let dir_path = "docs.gl/gl4";
-    // let dir = fs::read_dir(dir_path)?;
-    // for file in dir{
-    //     let file_path = file?.path();
-    //     let content = read_entire_xml_file(&file_path)?;
-    //     println!("{file_path:?} => {size}",size = content.len());
+    let dir_path = "docs.gl/gl4";
+    let dir = fs::read_dir(dir_path)?;
+    // let top_n = 20;
+    let mut tf_index = TermFreqIndex::new();
+    for file in dir{
+        let file_path = file?.path();
+        println!("Indexing {:?}...",&file_path);
+        // let file_path = "docs.gl/gl4/glVertexAttribDivisor.xhtml";
+        let content = read_entire_xml_file(&file_path)?.chars().collect::<Vec<_>>();
+    
+        let mut tf = TermFreq::new();
+    
+    
+    
+        for token in Lexer::new(&content){
+            let term = token.iter().map(|x|x.to_ascii_uppercase()).collect::<String>();
+            
+            if let Some(freq) = tf.get_mut(&term){
+                *freq+=1;
+            }else {
+                
+                    tf.insert(term,1);
+                
+            }
+            // println!("{token:?}");
+        }
+    
+        let mut stats = tf.iter().collect::<Vec<_>>();
+        stats.sort_by_key(|(_,f)| *f);
+        stats.reverse();
+        tf_index.insert(file_path,tf);
+        
 
+    }
+    let index_path = "index.json";
+    println!("Saving {index_path}...");
+    let index_file = File::create(index_path)?;
+    serde_json::to_writer(index_file,&tf_index).expect("Serde works fine");
+
+
+
+    // save to json format 
+
+
+
+    // for (path,tf) in tf_index{
+    //     println!("{path:?} has {count} with unique tokesn",count =tf.len());
     // }
         // println!("{content}",content = read_entire_xml_file(file_path).expect("TODO"));
         Ok(())
